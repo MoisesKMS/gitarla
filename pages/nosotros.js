f76afa8681cba1bb5bd224a0dda98ca86ccdd91a
Moises Jimenez <MoisesJimenez@Yandex.com>
2022-03-14 19:50:11 -0600

    0.0.0
    Init Commit

---
+++ pages/nosotros.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Nosotros = () => {
+    return (
+        <>
+            <h1>Desde Nosotros</h1>
+            <Link href="/">Ir a Nosotros</Link>
+        </>
+    )
+}
+export default Nosotros;
